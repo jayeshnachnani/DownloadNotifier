4ae54491378ac629c4230992d9207da3d4bfa808
Jayesh Nachnani <jayesh.nachnani@gmail.com>
2021-02-16 17:27:10 +0530

    added back button from to detail screen

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,15 +1,24 @@
 package com.udacity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
+    private lateinit var button1: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
-    }
 
+        button1 = findViewById(R.id.buttonTest)
+        button1.setOnClickListener {
+            val intent = Intent(this.applicationContext, MainActivity::class.java);
+            startActivity(intent);
+        }
+    }
 }
